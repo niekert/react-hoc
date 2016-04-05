98d2d63542c915c7236f54df98cec6b264172506
niekert <niekkruse70@gmail.com>
2016-04-05 22:07:02 +0200

    React Higher action authentication component

---
+++ src/components/Resources.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default () => (
+  <div>
+    Super special secret recipe
+    <ul>
+      <li>1 Cup sugar</li>
+      <li>1 Cup pepper</li>
+      <li>1 Cup salt</li>
+    </ul>
+  </div>
+);
